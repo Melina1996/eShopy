45865cd332c88b10326f9bdc7d292df7140f7ba7
Melina1996 <melina.schlachter@gmx.de>
2024-02-20 13:55:46 +0100

    added quantity function

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,65 @@ import AllCards from './components/AllCards'
 
 function App() {
 
+  const myArticles = []
+
+  const mySingleArticles = []
+
+  const[plants,setPlants] = useState([
+    {
+        "name": "LATIA",
+        "price": 15,
+        "stock": 10,
+        "category": "indoor",
+        "img": "../src/assets/img/PLANT_ONE.jpg",
+    },
+    {
+        "name": "SUCCA",
+        "price": 25,
+        "stock": 8,
+        "category": "outdoor",
+        "img": "../src/assets/img/PLANT_TWO.jpg",
+    }
+  ])
+
+  let initialQuantities=[0,0,0,0,0,0,0,0,0,0,0,0]
+
+  const[quantity,setQuantity] = useState(initialQuantities)
+
+  function checkExist(index){
+
+    const newQuantities= quantity.map((c,i)=>{
+      if (i===index){
+        console.log("hi")
+        return c+1
+      } else {
+        return c
+      }
+    })
+
+    setQuantity(newQuantities)
+
+    console.log(quantity)
+
+
+  }
+
+
+  function changeBasket(article,id){
+    // myArticles.push(article)
+    checkExist(id)
+
+    // console.log(mySingleArticles)
+    // console.log(myArticles)
+    // countItems(myArticles)
+    // console.log(amount)
+  }
 
 
   return (
     <div className='w-screen h-max flex justify-center items-center bg-[white]'>
 
-      <AllCards />
+      <AllCards quantity={quantity} changeBasket={changeBasket} plants={plants} mySingleArticles={mySingleArticles}/>
       
     </div>
   )
--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import Card from './Card'
 import data from "../assets/JSON/data.json"
 
-export default function AllCards() {
+export default function AllCards(props) {
   return (
     <div className='w-[90%] lg:w-[90%] xl:w-[80%] h-[100%] bg-white flex justify-center items-center flex-wrap gap-6'>
       {/* loope über meine Elemente des Arrays, um mir alle anzeigen zu lassen */}
-      {data.map((element, id) => {
+      {props.plants.map((element, id) => {
               return (
-                <Card name={element.name} price={element.price} stock={element.stock} category={element.category} img={element.img}/>
+                <Card key={id} id={id} quantity={props.quantity} changeBasket={props.changeBasket} element={element}/>
               )
               })
           }     
 
-      
     </div>
+
+
   )
 }
