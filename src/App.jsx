f14c91ca62d4dc6d0604e49d479308a1805f481c
Melina1996 <melina.schlachter@gmx.de>
2024-02-20 14:10:42 +0100

    added all plants

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,77 @@ function App() {
         "stock": 8,
         "category": "outdoor",
         "img": "../src/assets/img/PLANT_TWO.jpg",
-    }
+    },
+    {
+      "name": "PLAVIA",
+      "price": 20,
+      "stock": 12,
+      "category": "indoor",
+      "img": "../src/assets/img/PLANT_THREE.jpg"
+  },
+  {
+      "name": "SAVAGIA",
+      "price": 30,
+      "stock": 8,
+      "category": "outdoor",
+      "img": "../src/assets/img/PLANT_FOUR.jpg"
+  },
+  {
+      "name": "SNAKIS",
+      "price": 18,
+      "stock": 5,
+      "category": "indoor",
+      "img": "../src/assets/img/PLANT_FIVE.jpg"
+  },
+  {
+      "name": "LAURE",
+      "price": 28,
+      "stock": 8,
+      "category": "indoor",
+      "img": "../src/assets/img/PLANT_SIX.jpg"
+  },
+  {
+      "name": "MARA",
+      "price": 20,
+      "stock": 20,
+      "category": "indoor",
+      "img": "../src/assets/img/PLANT_SEVEN.jpg"
+  },
+  {
+      "name": "CIELA",
+      "price": 12,
+      "stock": 2,
+      "category": "outdoor",
+      "img": "../src/assets/img/PLANT_EIGHT.jpg"
+  },
+  {
+      "name": "TIZIA",
+      "price": 33,
+      "stock": 13,
+      "category": "indoor",
+      "img": "../src/assets/img/PLANT_NINE.jpg"
+  },
+  {
+      "name": "SUCCULENTA",
+      "price": 12,
+      "stock": 35,
+      "category": "indoor",
+      "img": "../src/assets/img/PLANT_TEN.jpg"
+  },
+  {
+      "name": "CACTUS",
+      "price": 5,
+      "stock": 3,
+      "category": "indoor",
+      "img": "../src/assets/img/PLANT_ELEVEN.jpg"
+  },
+  {
+      "name": "COLARE",
+      "price": 20,
+      "stock": 3,
+      "category": "indoor",
+      "img": "../src/assets/img/PLANT_TWELVE.jpg"
+  }
   ])
 
   let initialQuantities=[0,0,0,0,0,0,0,0,0,0,0,0]
