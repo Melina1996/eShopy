d8b4e8fd5ee0ee8b3dd42861435fc0950f80b84c
Melina1996 <melina.schlachter@gmx.de>
2024-02-20 14:05:46 +0100

    cleaned app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import Basket from './components/Basket'
 
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
 
-      <Basket />
+
       
     </div>
   )
