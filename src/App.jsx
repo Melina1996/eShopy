c63941f932bbc15fd54c9df7c170418e186cd0be
Melina1996 <melina.schlachter@gmx.de>
2024-02-20 08:25:32 +0100

    added structure basket

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Basket from './components/Basket'
 
 
 function App() {
 
   return (
-    <div className='w-screen h-screen'>
+    <div className='w-screen h-screen flex justify-center items-center'>
+
+      <Basket />
       
     </div>
   )
--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import ItemBasekt from './itemBasekt'
 
 export default function Basket() {
   return (
-    <div>Basket</div>
+    <div className='w-[screen] h-[screen] bg-[white] flex justify-center items-center'>
+
+      <div className='w-[90%] h-[100%]'>
+
+        
+
+      </div>
+
+      <div className='w-[10%] h-[100%] flex justify-start items-start'>
+
+        <p>X</p>
+
+      </div>
+
+
+    </div>
   )
 }
